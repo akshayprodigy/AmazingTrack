8b16eb467429779181c1051ae0c27932188eefde
akshayprodigy <i.akshayghosh@gmail.com>
2024-10-25 10:19:22 +0530

    updated with ads mediation

--- Assets/Scripts/Ads/UnityAdsManager.cs
+++ Assets/Scripts/Ads/UnityAdsManager.cs
@@ -4,10 +4,26 @@ using System.Collections.Generic;
 public class UnityAdsManager : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public static UnityAdsManager Instance { get; private set; }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Awake()
+    {
+        // Check if instance already exists and enforce the singleton pattern
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject); // Destroy duplicate instance
+            return;
+        }
+
+        Instance = this;
+        DontDestroyOnLoad(gameObject); // Persist between scenes
+    }
+    
     void Start()
     {
         #if UNITY_ANDROID
-        string appKey = "85460dcd";
+        string appKey = "85460dcd"; // Replace with your own app key 1fee20f75
 #elif UNITY_IPHONE
         string appKey = "8545d445";
 #else
@@ -25,9 +41,161 @@ public class UnityAdsManager : MonoBehaviour
         IronSource.Agent.init(appKey);
     }
 
-    // Update is called once per frame
-    void Update()
+
+    void OnEnable()
     {
-        
+        //Add Init Event
+        IronSourceEvents.onSdkInitializationCompletedEvent += SdkInitializationCompletedEvent;
+
+        //Add ImpressionSuccess Event
+        IronSourceEvents.onImpressionDataReadyEvent += ImpressionDataReadyEvent;
+
+        //Add AdInfo Rewarded Video Events
+        IronSourceRewardedVideoEvents.onAdOpenedEvent += RewardedVideoOnAdOpenedEvent;
+        IronSourceRewardedVideoEvents.onAdClosedEvent += RewardedVideoOnAdClosedEvent;
+        IronSourceRewardedVideoEvents.onAdAvailableEvent += RewardedVideoOnAdAvailable;
+        IronSourceRewardedVideoEvents.onAdUnavailableEvent += RewardedVideoOnAdUnavailable;
+        IronSourceRewardedVideoEvents.onAdShowFailedEvent += RewardedVideoOnAdShowFailedEvent;
+        IronSourceRewardedVideoEvents.onAdRewardedEvent += RewardedVideoOnAdRewardedEvent;
+        IronSourceRewardedVideoEvents.onAdClickedEvent += RewardedVideoOnAdClickedEvent;
+
+        //Add AdInfo Interstitial Events
+        IronSourceInterstitialEvents.onAdReadyEvent += InterstitialOnAdReadyEvent;
+        IronSourceInterstitialEvents.onAdLoadFailedEvent += InterstitialOnAdLoadFailed;
+        IronSourceInterstitialEvents.onAdOpenedEvent += InterstitialOnAdOpenedEvent;
+        IronSourceInterstitialEvents.onAdClickedEvent += InterstitialOnAdClickedEvent;
+        IronSourceInterstitialEvents.onAdShowSucceededEvent += InterstitialOnAdShowSucceededEvent;
+        IronSourceInterstitialEvents.onAdShowFailedEvent += InterstitialOnAdShowFailedEvent;
+        IronSourceInterstitialEvents.onAdClosedEvent += InterstitialOnAdClosedEvent;
+
+        //Add AdInfo Banner Events
+       
     }
+
+    void OnApplicationPause(bool isPaused)
+    {
+        Debug.Log("unity-script: OnApplicationPause = " + isPaused);
+        IronSource.Agent.onApplicationPause(isPaused);
+    }
+
+
+    #region Init callback handlers
+
+    void SdkInitializationCompletedEvent()
+    {
+        Debug.Log("unity-script: I got SdkInitializationCompletedEvent");
+    }
+
+    #endregion
+
+    #region AdInfo Rewarded Video
+    void RewardedVideoOnAdOpenedEvent(IronSourceAdInfo adInfo)
+    {
+        Debug.Log("unity-script: I got RewardedVideoOnAdOpenedEvent With AdInfo " + adInfo);
+    }
+
+    void RewardedVideoOnAdClosedEvent(IronSourceAdInfo adInfo)
+    {
+        Debug.Log("unity-script: I got RewardedVideoOnAdClosedEvent With AdInfo " + adInfo);
+    }
+
+    void RewardedVideoOnAdAvailable(IronSourceAdInfo adInfo)
+    {
+        Debug.Log("unity-script: I got RewardedVideoOnAdAvailable With AdInfo " + adInfo);
+    }
+
+    void RewardedVideoOnAdUnavailable()
+    {
+        Debug.Log("unity-script: I got RewardedVideoOnAdUnavailable");
+    }
+
+    void RewardedVideoOnAdShowFailedEvent(IronSourceError ironSourceError, IronSourceAdInfo adInfo)
+    {
+        Debug.Log("unity-script: I got RewardedVideoOnAdShowFailedEvent With Error" + ironSourceError + "And AdInfo " + adInfo);
+    }
+
+    void RewardedVideoOnAdRewardedEvent(IronSourcePlacement ironSourcePlacement, IronSourceAdInfo adInfo)
+    {
+        Debug.Log("unity-script: I got RewardedVideoOnAdRewardedEvent With Placement" + ironSourcePlacement + "And AdInfo " + adInfo);
+    }
+
+    void RewardedVideoOnAdClickedEvent(IronSourcePlacement ironSourcePlacement, IronSourceAdInfo adInfo)
+    {
+        Debug.Log("unity-script: I got RewardedVideoOnAdClickedEvent With Placement" + ironSourcePlacement + "And AdInfo " + adInfo);
+    }
+
+    #endregion
+
+
+    #region AdInfo Interstitial
+
+    void InterstitialOnAdReadyEvent(IronSourceAdInfo adInfo)
+    {
+        Debug.Log("unity-script: I got InterstitialOnAdReadyEvent With AdInfo " + adInfo);
+    }
+
+    void InterstitialOnAdLoadFailed(IronSourceError ironSourceError)
+    {
+        Debug.Log("unity-script: I got InterstitialOnAdLoadFailed With Error " + ironSourceError);
+    }
+
+    void InterstitialOnAdOpenedEvent(IronSourceAdInfo adInfo)
+    {
+        Debug.Log("unity-script: I got InterstitialOnAdOpenedEvent With AdInfo " + adInfo);
+    }
+
+    void InterstitialOnAdClickedEvent(IronSourceAdInfo adInfo)
+    {
+        Debug.Log("unity-script: I got InterstitialOnAdClickedEvent With AdInfo " + adInfo);
+    }
+
+    void InterstitialOnAdShowSucceededEvent(IronSourceAdInfo adInfo)
+    {
+        Debug.Log("unity-script: I got InterstitialOnAdShowSucceededEvent With AdInfo " + adInfo);
+    }
+
+    void InterstitialOnAdShowFailedEvent(IronSourceError ironSourceError, IronSourceAdInfo adInfo)
+    {
+        Debug.Log("unity-script: I got InterstitialOnAdShowFailedEvent With Error " + ironSourceError + " And AdInfo " + adInfo);
+    }
+
+    void InterstitialOnAdClosedEvent(IronSourceAdInfo adInfo)
+    {
+        Debug.Log("unity-script: I got InterstitialOnAdClosedEvent With AdInfo " + adInfo);
+    }
+
+    #endregion
+
+
+    #region ImpressionSuccess callback handler
+
+    void ImpressionDataReadyEvent(IronSourceImpressionData impressionData)
+    {
+        Debug.Log("unity - script: I got ImpressionDataReadyEvent ToString(): " + impressionData.ToString());
+        Debug.Log("unity - script: I got ImpressionDataReadyEvent allData: " + impressionData.allData);
+    }
+
+    #endregion
+    // Update is called once per frame
+        public void ShowRewardedVideo(){
+                if (IronSource.Agent.isRewardedVideoAvailable())
+                {
+                        IronSource.Agent.showRewardedVideo();
+                }
+                else
+                {
+                        Debug.Log("unity-script: IronSource.Agent.isRewardedVideoAvailable - False");
+                }
+        }
+
+        public void ShowInterstitial(){
+                if (IronSource.Agent.isInterstitialReady())
+                {
+                        IronSource.Agent.showInterstitial();
+                }
+                else
+                {
+                        Debug.Log("unity-script: IronSource.Agent.isInterstitialReady - False");
+                }
+        }
 }
